48f89921fbc3dd68a470937609e3f8180ffc6b4e
gonzaareal17-dot <gonzaareal17@gmail.com>
2026-07-22 01:53:55 -0300

    Update PP.ino

--- PP.ino
+++ PP.ino
@@ -1,11 +1,29 @@
 #include <LedControl.h>
+#include <TM1637Display.h>
+#include <TimeLib.h>
+
+#define CLK 6
+#define DIO 7
+
+TM1637Display reloj(CLK, DIO);
+
+const int minutos = 45;
+const int segundos = 0;
+time_t finalperiodo;
+bool cronometro = true;
 
 LedControl lc = LedControl(13, 11, 12, 1);
+
+int periodos = 1;
 
 const int MASLOCAL = 2;
 const int MENOSLOCAL = 3;
 const int MASVIS = 4;
 const int MENOSVIS = 5;
+const int MAS10 = A0;
+const int MAS5 = A1;
+const int inicio = A2;
+const int reinicio = A3;
 
 int local = 0;
 int visitante = 0;
@@ -22,6 +40,8 @@ void MARCADOR()
 
   lc.setDigit(0, 3, visitante / 10, false);
   lc.setDigit(0, 2, visitante % 10, false);
+
+  lc.setDigit(0, 4, periodos, false);
 }
 void setup()
 {
@@ -31,13 +51,18 @@ void setup()
   pinMode(MENOSVIS, INPUT_PULLUP);
 
   lc.shutdown(0, false);
+  lc.setScanLimit(0, 4);
   lc.setIntensity(0, 8);
   lc.clearDisplay(0);
 
+  reloj.setBrightness(5); 
+  setTime(0, 0, 0, 1, 1, 2026);
+  time_t tiempototal= (minutos * 60) + segundos;
+  finalperiodo = now() + tiempototal;
+
   MARCADOR();
 }
-void loop()
-{
+void loop(){
   bool ML = digitalRead(MASLOCAL);
   bool MEL = digitalRead(MENOSLOCAL);
   bool MV = digitalRead(MASVIS);
@@ -73,4 +98,23 @@ void loop()
     MEVANT = MEV;
 
     delay(25);
+    if (cronometro){
+      time_t tiemporeal = now();
+      if (tiemporeal < finalperiodo){
+        time_t tiemporestante = finalperiodo - tiemporeal;
+        int Minutos = tiemporestante / 60;
+        int Segundos = tiemporestante % 60;
+        int mostrar = (Minutos * 100) + Segundos;
+        reloj.showNumberDecEx(mostrar, 0b01000000, true);
+      }
+    else{
+      cronometro = false;
+      periodos = 1;
+      reloj.showNumberDec(0, true);
+      periodos++;
+      if(periodos > 9) { 
+      }
+      MARCADOR();
+    }
+  }
 }
